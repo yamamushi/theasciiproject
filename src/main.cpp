0ba55ecb648bf618e25fe751215765dda14cd9d3
yamamushi <yamamushi@gmail.com>
2012-07-03 03:06:22 -0500

    Source Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  The ASCII Project main loop
+ *
+ *        Version:  1.0
+ *        Created:  07/03/2012 03:04:46 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yamamushi (Jon Rumion)
+ *   Organization:  The ASCII Project
+ *
+ * =====================================================================================
+ */
+
+// First our custom headers
 #include "libtcod.hpp"
 #include "keyboard.h"
+
+// Followed by standard headers
+#include <math.h>
+
 // size of the heightmap
 #define MAIN_WIDTH 100
 #define MAIN_HEIGHT 80
-#define LIMIT_FPS 20
-#include <math.h>
 
+#define LIMIT_FPS 20
+
+// We'll probably move these global variables somewhere else later
 int playerX, playerY;
 
 
