b867fc7c78385059fbc742a91d3a809409009a4c
yamamushi <yamamushi@gmail.com>
2012-09-30 23:32:37 -0500

    Initial Commit

---
+++ Legends of Meru/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Legends of Meru
+//
+//  Created by Jonathan Rumion on 9/30/12.
+//  Copyright (c) 2012 Jonathan Rumion. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
