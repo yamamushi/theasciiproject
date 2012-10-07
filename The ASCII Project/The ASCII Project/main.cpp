a734b0f440fa9a6828cdee458862526d67a2df25
yamamushi <yamamushi@gmail.com>
2012-10-07 16:03:25 -0500

    Updates

---
+++ The ASCII Project/The ASCII Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  The ASCII Project
+//
+//  Created by Jonathan Rumion on 10/7/12.
+//  Copyright (c) 2012 The ASCII Project. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
