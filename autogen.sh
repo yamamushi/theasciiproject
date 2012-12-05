b8063d8dff5aaf83c8264d1271771ef59f5ceb79
Yamamushi <yamamushi@gmail.com>
2012-12-05 12:22:37 -0600

    housekeeping

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 
-test -n "$srcdir/build-aux" || srcdir=`dirname "$0/build-aux"`
-test -n "$srcdirf" || srcdir=./build-aux
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
 
 make -k
 
-## Leaving this out to allow for "make install" to work properly
-# mv tap_server bin/
+## Leave this out to allow for "make install" to work properly
+mv tap_server bin/
 
 ## Uncomment to get a clean build process
-# make distclean
+make distclean
