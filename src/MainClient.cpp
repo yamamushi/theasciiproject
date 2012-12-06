c21ab6849f61641026a7512e6dbfa07873c7bf38
Yamamushi <yamamushi@gmail.com>
2012-12-05 21:37:13 -0600

    Updating

---
+++ src/MainClient.cpp
@@ -0,0 +1,24 @@
+/*
+
+  The ASCII Project - Client
+
+  December 5th, 2012
+  -Yamamushi@gmail.com
+
+  
+  Our portable SDL-based client
+
+
+ */
+
+
+#include <iostream>
+
+
+int main(int argc, char *argv[]){
+
+  std::cout << "Testing Client" << std::endl;
+
+  return 0;
+
+}
