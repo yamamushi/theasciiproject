b1bacb77b1df38736e4fbf1ea08679bcfe9a0ba0
Yamamushi <yamamushi@gmail.com>
2012-12-05 22:43:09 -0600

    Automake SDL

--- src/MainClient.cpp
+++ src/MainClient.cpp
@@ -13,6 +13,14 @@
 
 
 #include <iostream>
+#include "SDL/SDL.h"
+#include "SDL/SDL_ttf.h"
+
+
+// Ignore this stuff, it's only here to test automake
+// ie, to make sure automake is linking sdl_* stuff properly.
+TTF_Font *font = NULL;
+SDL_Color textcolor = {255, 255, 255};
 
 
 int main(int argc, char *argv[]){
--- src/MainServer.cpp
+++ src/MainServer.cpp
@@ -14,11 +14,12 @@
 #include <iterator>
 #include <algorithm>
 
-int main()
+int main(int argc, char *argv[])
 {
     using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 
-    std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+    std::for_each( in(std::cin), in(), std::cout << (_1 * 3) << " " );
+
+    return 0;
 }
