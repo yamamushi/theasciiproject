7c5f5b278f955b7a2e555bbeb70975d00cc4ae64
Yamamushi <yamamushi@gmail.com>
2013-02-24 21:17:21 -0600

    Migrating back to Xcode

---
+++ src/mapping/Tile.h
@@ -0,0 +1,34 @@
+/*
+
+  Tile.h
+  2-24-2013
+
+  -Jon Rumion (Yamamushi@gmail.com)
+
+
+  This is going to be the source of a lot of headaches.
+
+ */
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/serialization/shared_ptr.hpp>
+
+
+class Tile {
+
+private:
+
+  
+
+  
+public:
+
+  Tile(){};
+  virtual ~Tile(){};
+
+};
