ab1fe88ef1558a708a7aa502f42570d338a29d16
Yamamushi <yamamushi@gmail.com>
2013-03-02 21:59:27 -0600

    WMapDump Utility
    WMapDump is a utility for dumping Global WorldMap information to console

---
+++ WMapDump/WMapDumpMain.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WMapDump
+//
+//  Created by Jonathan Rumion on 3/2/13.
+//  Copyright (c) 2013 TAP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
