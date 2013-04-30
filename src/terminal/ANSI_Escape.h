1275661903250f4bd33ab9b2d96cc4dbf4a638b0
Yamamushi <yamamushi@gmail.com>
2013-04-30 14:04:14 -0500

    ANSI Escape Sequences
    Nothing here, just creating the file

---
+++ src/terminal/ANSI_Escape.h
@@ -0,0 +1,19 @@
+//
+//  ANSI_Escape.h
+//  The ASCII Project
+//
+//  Created by Jonathan Rumion on 4/30/13.
+//  Copyright (c) 2013 TAP. All rights reserved.
+//
+//  Our ANSI Escape Sequence Definitions
+//
+//  For More Info, please see: http://en.wikipedia.org/wiki/ANSI_escape_code
+//  This is also a good reference: http://ascii-table.com/ansi-escape-sequences.php
+//
+
+#ifndef The_ASCII_Project_ANSI_Escape_h
+#define The_ASCII_Project_ANSI_Escape_h
+
+
+
+#endif
